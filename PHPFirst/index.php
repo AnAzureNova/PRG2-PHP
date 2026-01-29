6a09aaf7b5855ffe315835038eb7eb04a7f74cb1
Matthew <realazurenova@gmail.com>
2026-01-29 08:46:50 +0100

    Another commit

--- PHPFirst/index.php
+++ PHPFirst/index.php
@@ -6,8 +6,9 @@
     <title>Document</title>
  </head>
  <body>
-    <form action="inputs.php" method="get">
+    <form action="inputs.php" method="post">
         <label for="text">TEXT: </label><input id="text" name="text" type="text">
+        <label for="range">RANGE: </label><input id="range" name="range" type="range">
     </form>
  </body>
  </html>
---
+++ PHPFirst/inputs.php
@@ -0,0 +1,4 @@
+<?php
+    echo "Text: ".$_POST["text"]."<br>";
+    echo "Range: ", $_POST[""]."<br>";
+?>
