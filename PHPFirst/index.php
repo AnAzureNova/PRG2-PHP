f4a951a0925d963ba51222a1d0e03414c71b461e
Matthew <realazurenova@gmail.com>
2026-01-29 08:39:43 +0100

    Another test

--- PHPFirst/index.php
+++ PHPFirst/index.php
@@ -1,1 +1,13 @@
- 
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <form action="inputs.php" method="get">
+        <label for="text">TEXT: </label><input id="text" name="text" type="text">
+    </form>
+ </body>
+ </html>
