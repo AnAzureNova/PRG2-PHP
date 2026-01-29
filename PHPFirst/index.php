bff41080cabeb66f326ff59f6f9ebbe03a163b7d
Matthew <realazurenova@gmail.com>
2026-01-29 09:22:06 +0100

    Radio buttons

--- PHPFirst/index.php
+++ PHPFirst/index.php
@@ -8,9 +8,12 @@
  <body>
     <form action="inputs.php" method="post">
         <label for="text">TEXT: </label><input id="text" name="text" type="text">
-        <label for="range">RANGE: </label><input id="range" name="range" type="range">
-        <label for="checkbox">CHECK: </label><input id="checkbox" name="checkbox" type="checkbox">
+        <label for="range">RANGE: </label><input id="range" name="range" type="range"><br>
+        <label for="checkbox">CHECK: </label><input id="checkbox" name="checkbox" type="checkbox"><br>
+        <label for="radio1">RADIO 1:</label><input id="radio1" name="radio" type="radio">
+        <label for="radio2">RADIO 2:</label><input id="radio2" name="radio" type="radio">
         <input type="submit">
+        <lable
     </form>
  </body>
  </html>
--- PHPFirst/inputs.php
+++ PHPFirst/inputs.php
@@ -3,4 +3,5 @@
     echo "Range: ". $_POST["range"]."<br>";
     $checkbox = $_POST["checkbox"] ?? "off";
     echo "Checkbox: ".$checkbox."<br>";
+    echo "Radio: ".$_POST["radio"]."<br>";
 ?>
