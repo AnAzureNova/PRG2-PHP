8d6851963fd246f9215c4fe780bd2e4856fb0cff
Matthew <realazurenova@gmail.com>
2026-01-29 18:35:51 +0100

    Further inputs

--- PHPFirst/index.php
+++ PHPFirst/index.php
@@ -9,11 +9,14 @@
  <body>
     <form action="inputs.php" method="post">
         <label for="text">TEXT: </label><input id="text" name="text" type="text">
-        <label for="range">RANGE: </label><input id="range" name="range" type="range"><br>
-        <label for="checkbox">CHECK: </label><input id="checkbox" name="checkbox" type="checkbox"><br>
+        <label for="range">RANGE: </label><input id="range" name="range" type="range">
+        <label for="checkbox">CHECK: </label><input id="checkbox" name="checkbox" type="checkbox">
         <label for="radio1">RADIO 1:</label><input id="radio1" name="radio" type="radio" value="1">
         <label for="radio2">RADIO 2:</label><input id="radio2" name="radio" type="radio" value="2">
         <label for="color">COLOR: </label><input id="color" name="color" type="color">
+        <label for="url"></label>URL: <input id="url" name="url" type="url">
+        <label for="date"></label>DATE: <input id="date" name="date" type="date">
+        <label for="tel"></label>TEL: <input id="tel" name="tel" type="tel">
         <input type="submit">
         <p>TEST</p>
     </form>
--- PHPFirst/inputs.php
+++ PHPFirst/inputs.php
@@ -6,4 +6,7 @@
     echo "Checkbox: ".$checkbox."<br>";
     echo "Radio: ".$_POST["radio"]."<br>";
     echo "Color: ".$_POST["color"]."<br>";
+    echo "Url: ".$_POST["url"]."<br>";
+    echo "Date: ".$_POST["date"]."<br>";
+    echo "Tel: ".$_POST["tel"]."<br>";
 ?>
