69c96e1bd3e1ed2f1efb7d4dda1a726a7bd14642
Matthew <realazurenova@gmail.com>
2026-02-09 15:16:22 +0100

    Yeah no

--- DU2/login.php
+++ DU2/login.php
@@ -6,6 +6,11 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div>
+        <h1>LOGIN</h1>
+        <form method="post" action="index.php">
+
+        </form>
+    </div>
 </body>
 </html>
